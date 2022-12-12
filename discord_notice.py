52fdddb2f38436b7bbef65f0b215cda9ebac512e
Hyuntaek Lim <33558083+devLupin@users.noreply.github.com>
2022-12-12 15:14:10 +0900

    add discord_notice.py

---
+++ discord_notice.py
@@ -0,0 +1,29 @@
+from urllib import request
+from xxlimited import foo
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from time import sleep
+from datetime import datetime
+
+webhook_url = "[your webhook url]"
+
+def send_message(title, description):
+    webhook = DiscordWebhook(url=webhook_url)
+    
+    embed = DiscordEmbed(title=title, description=description, color='03b2f8')
+    embed.set_author(name='status', icon_url='[your icon url]')
+    embed.set_timestamp()
+
+    webhook.add_embed(embed)
+    status = webhook.execute()
+
+def start():
+    now = datetime.now()
+    msg = now.strftime('%Y-%m-%d %H:%M:%S')
+    send_message(title='is running', description=f'{msg}')
+    return
+
+def end():
+    now = datetime.now()
+    msg = now.strftime('%Y-%m-%d %H:%M:%S')
+    send_message(title='is done', description=f'{msg}')
+    return
